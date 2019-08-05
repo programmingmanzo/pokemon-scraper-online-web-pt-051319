d38999349bcee66273451174bc7ad3a4de7154ee
Gerardo Manzo <programmingmanzo@gmail.com>
2019-08-05 02:31:49 +0000

    Automatically backed up by Learn

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,8 +1,4 @@
 -- Write your sql commands to create your db schema here.
 -- It should create a table in a new table in the db/ directory.
 
-CREATE TABLE pokemon (
-  id integer primary key autoincrement,
-  name text,
-  type text
-);
+CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT);
